48bfe03e5e8f113abb733817d72a6b1eddaa49c0
scotthvid <148949305+scotthvid@users.noreply.github.com>
2024-05-23 14:26:25 +0200

    Misc

--- QRkode test/qrkode_generate.py
+++
@@ -1,6 +0,0 @@
-import qrcode
-img = qrcode.make('Google.com')
-type(img) 
-img.save("qrcode1.png")
-
-#Link til kodehjælp: https://medium.com/@reegan_anne/fully-customizable-qr-codes-in-python-7eb8a7c3b0da
